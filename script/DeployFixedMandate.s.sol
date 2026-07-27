88ef155a92bee8d8b1b1b38b70e764a3abccd7bf
Alex Roan <alex@cyfrin.io>
2026-07-27 20:28:27 +0100

    create2

--- script/DeployFixedMandate.s.sol
+++ script/DeployFixedMandate.s.sol
@@ -3,11 +3,22 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {FixedMandate} from "../src/FixedMandate.sol";
+import {console} from "forge-std/console.sol";
 
 contract DeployFixedMandate is Script {
-    function run() external returns (FixedMandate executor) {
+    bytes32 public constant MANDATE_SALT = keccak256("MANDATE");
+
+    function run() external returns (FixedMandate mandate) {
+        address predictedAddress = vm.computeCreate2Address(MANDATE_SALT, keccak256(type(FixedMandate).creationCode));
+
         vm.startBroadcast();
-        executor = new FixedMandate();
+        if (predictedAddress.code.length == 0) {
+            mandate = new FixedMandate{salt: MANDATE_SALT}();
+        } else {
+            mandate = FixedMandate(predictedAddress);
+        }
+        require(predictedAddress == address(mandate), "Deployment failed");
+        console.log("Deployed mandate to", address(mandate));
         vm.stopBroadcast();
     }
 }
